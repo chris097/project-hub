8df50a5791c94f2a1dc0a939005dbc14f66eb02e
chris097 <chrisfidel.international@gmail.com>
2024-01-20 00:19:59 +0100

    working

--- src/app/pages/drag-and-drop/page.tsx
+++ src/app/pages/drag-and-drop/page.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react';
+import Header from '@/components/Header/page';
 
 const DragAndDrop = () => {
+  const [file, setFile] = useState<File | undefined>();
+
+  const handleFileUploadSubmit = async(e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    if (typeof file === 'undefined') return;
+
+    const formData = new FormData();
+
+    formData.append('file', file);
+    
+  };
+
+  const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement & {
+      files: FileList
+    };
+    const file = new FileReader;
+
+    file.onload = function () {
+      if (typeof file.result !== 'string') return;
+
+      setFile(target.files[0]);
+    };
+    
+    file.readAsDataURL(target.files[0])
+  }
+
   return (
-    <div>DragAndDrop</div>
+    <div>
+      <Header />
+      <form onSubmit={handleFileUploadSubmit} className='flex justify-center flex-col w-full items-center'>
+        <div>
+          <input
+            name='image'
+            accept='image/png, image/jpg'
+            type='file'
+            onChange={handleOnChange}
+          />
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
