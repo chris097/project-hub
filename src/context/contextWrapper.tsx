f3eb0757216666f758ec6e2526c50b15155b753d
chris097 <chrisfidel.international@gmail.com>
2024-01-25 00:01:49 +0100

    calendar

--- src/context/contextWrapper.tsx
+++ src/context/contextWrapper.tsx
@@ -3,12 +3,12 @@ import GlobalContext from './GlobalContext';
 import dayjs from 'dayjs';
 
 const ContextWrapper = (props: any) => {
-    const [monthIndex, setMonthIndex] = useState(dayjs().month());
+  const [monthIndex, setMonthIndex] = useState(dayjs().month());
 
-  return  <GlobalContext.Provider value={{monthIndex, setMonthIndex}}>
-          {props.children}
-    </GlobalContext.Provider>
+  return <GlobalContext.Provider value={{ monthIndex, setMonthIndex }}>
+    {props.children}
+  </GlobalContext.Provider>
   
-}
+};
 
 export default ContextWrapper;
